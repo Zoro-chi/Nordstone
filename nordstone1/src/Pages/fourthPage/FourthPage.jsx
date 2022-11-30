29f84e12dde192f524505516892ec110bc1832a1
Zoro-chi <jagbetuyi001@gmail.com>
2022-11-30 15:00:49 +0100

    slider feature

--- nordstone1/src/Pages/fourthPage/FourthPage.jsx
+++ nordstone1/src/Pages/fourthPage/FourthPage.jsx
@@ -1,96 +1,116 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Slider } from 'primereact/slider';
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 import "./fourthPage.scss";
 import Nav from "../../components/nav/Nav";
 import { calculation } from "../../config/api";
 
 const FourthPage = ({ user, setUser }) => {
-  const [answer, setAnswer] = useState("");
-  const [num1, setNum1] = useState("");
-  const [num2, setNum2] = useState("");
-  const [operator, setOperator] = useState("");
-  const navigate = useNavigate();
+	const [answer, setAnswer] = useState("");
+	const [num1, setNum1] = useState("");
+	const [num2, setNum2] = useState("");
+	const [operator, setOperator] = useState("");
+	const navigate = useNavigate();
 
-  const handleClick = async (e) => {
-    e.preventDefault();
-    try {
-      const calculate = await calculation(num1, num2, operator);
-      setAnswer(calculate.data.answer);
-    } catch (error) {
-      setAnswer("Please fill all fields");
-      console.log(error.message);
-    }
-  };
+	const handleClick = async (e) => {
+		e.preventDefault();
+		try {
+			const calculate = await calculation(num1, num2, operator);
+			setAnswer(calculate.data.answer);
+		} catch (error) {
+			setAnswer("Please fill all fields");
+			console.log(error.message);
+		}
+	};
 
-  return (
-    <div className="containerCalc">
-      <Nav user={user} setUser={setUser} />
-      {/* <div className="calculation">
-        <form className="inputs">
-          <input
-            placeholder="Enter a number"
-            type="number"
-            name="num1"
-            onChange={(e) => setNum1(e.target.value)}
-            required
-          />
-          <select
-            placeholder="Select an operator"
-            name="operator"
-            onChange={(e) => setOperator(e.target.value)}
-            required
-          >
-            <option value=""> </option>
-            <option value="+"> + </option>
-            <option value="-"> - </option>
-            <option value="*"> * </option>
-          </select>
-          <input
-            placeholder="Enter a number"
-            type="number"
-            name="num2"
-            onChange={(e) => setNum2(e.target.value)}
-            required
-          />
-          <button onClick={handleClick} className="calcBtn" type="submit">
-            Calculate
-          </button>
-        </form>
-      </div> */}
+	useEffect(() => {
+		console.log(num1);
+		console.log(num2);
+	}, [num1, num2]);
 
-      {/* <div className="slider"> */}
-        
-          <Slider value={num1} onChange={(e) => setNum1(e.value)} min={0} max={100} range={100}/>
-          <select
-            placeholder="Select an operator"
-            name="operator"
-            onChange={(e) => setOperator(e.target.value)}
-            required
-          >
-            <option value=""> </option>
-            <option value="+"> + </option>
-            <option value="-"> - </option>
-            <option value="*"> * </option>
-          </select>
-          <Slider value={num2} onChange={(e) => setNum2(e.value)} min={0} max={100}/>
-          <button onClick={handleClick} className="calcBtn" type="submit">
-            Calculate
-          </button>
-      {/* </div> */}
+	return (
+		<div className="containerCalc">
+			<Nav user={user} setUser={setUser} />
+			{/* // ! FOR INPUT CALCULATION */}
+			{/* <div className="calculation">
+				<form className="inputs">
+					<input
+						placeholder="Enter a number"
+						type="number"
+						name="num1"
+						onChange={(e) => setNum1(e.target.value)}
+						required
+					/>
+					<select
+						placeholder="Select an operator"
+						name="operator"
+						onChange={(e) => setOperator(e.target.value)}
+						required
+					>
+						<option value=""> </option>
+						<option value="+"> + </option>
+						<option value="-"> - </option>
+						<option value="*"> * </option>
+					</select>
+					<input
+						placeholder="Enter a number"
+						type="number"
+						name="num2"
+						onChange={(e) => setNum2(e.target.value)}
+						required
+					/>
+					<button onClick={handleClick} className="calcBtn" type="submit">
+						Calculate
+					</button>
+				</form>
+			</div> */}
+			{/* // ! FOR SLIDER CALCULATION */}
+			<div className="slider">
+				<Slider
+					step={1}
+					min={0}
+					max={1000}
+					dots={false}
+					onChange={(value) => setNum1(value)}
+				/>
+				<span>First number: {num1}</span>
 
-      {answer && (
-        <span className="answer">
-          Your answer is: <p> {answer} </p>
-        </span>
-      )}
+				<select
+					placeholder="Select an operator"
+					name="operator"
+					onChange={(e) => setOperator(e.target.value)}
+					required
+				>
+					<option value=""> </option>
+					<option value="+"> + </option>
+					<option value="-"> - </option>
+					<option value="*"> * </option>
+				</select>
 
-      <span className="page" onClick={() => navigate("/thirdPage")}>
-        Prev Page
-      </span>
-    </div>
-  );
+				<Slider
+					step={1}
+					min={0}
+					max={1000}
+					dots={false}
+					onChange={(value) => setNum2(value)}
+				/>
+				<span>Second number: {num2}</span>
+				<button onClick={handleClick} className="calcBtn" type="submit">
+					Calculate
+				</button>
+			</div>
+			{answer && (
+				<span className="answer">
+					Your answer is: <p> {answer} </p>
+				</span>
+			)}
+			<span className="page" onClick={() => navigate("/thirdPage")}>
+				Prev Page
+			</span>
+		</div>
+	);
 };
 
 export default FourthPage;
--- nordstone1/src/config/api.js
+++ nordstone1/src/config/api.js
@@ -1,13 +1,25 @@
 import axios from "axios";
 
+const getBaseUrl = () => {
+	let url;
+	switch (process.env.NODE_ENV) {
+		case "production":
+			url = process.env.REACT_APP_API_URL_PROD;
+			break;
+
+		case "development":
+			url = process.env.REACT_APP_API_URL;
+	}
+	return url;
+};
+
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  //   baseURL: "http://localhost:8080",
+	baseURL: getBaseUrl(),
 });
 
 export const calculation = (num1, num2, operator) =>
-  API.post("/api/calc", {
-    num1,
-    operator,
-    num2,
-  });
+	API.post("/api/calc", {
+		num1,
+		operator,
+		num2,
+	});
